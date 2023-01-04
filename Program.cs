80e4b7e72ae33a0c4f0dc984606aa0920ead162a
yuliya <avdyukova.y.a@gmail.com>
2023-01-04 20:02:56 +0300

    итог

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿/*string[] array1 = new string[5] {"1234", "23", "world", "hello"}; 
+string[] array2 = new string [array1.length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.length; i++);
+    {
+        if(array1[i].length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array [i]}");
+    }
+    Console.Writeline();
+    SecondArrayWithif(array1, array2);
+}
+PrintArray(array2);*/
